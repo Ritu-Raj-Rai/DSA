96599391b38d008f79e797924e10d739ff6707d3
Ritu-Raj-Rai <rairituraj12@gmail.com>
2026-07-09 00:18:01 +0530

    Time: 1 ms (96.29%) | Memory: 45 MB (90.91%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -10,13 +10,11 @@ class Solution {
   int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[index] = nums[i];
-       index++;
+                nums[index++] = nums[i];
             }
         }
         while (index < nums.length) {
-            nums[index] = 0;
-            index++;
+            nums[index++] = 0;
         }
 
         return nums;
