942da9d59fc1c587d10c842ee069fca336f92a41
Ritu-Raj-Rai <rairituraj12@gmail.com>
2026-07-10 11:22:26 +0530

    Time: 5 ms (39.91%) | Memory: 43.7 MB (13.64%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,20 +1,8 @@
 class Solution {
-    public int[] sort(int[] heights) {
-    int n = heights.length;
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (heights[j] < heights[minIndex]) {
-                minIndex = j;
-            }
-        }
-        int temp = heights[minIndex];
-        heights[minIndex] = heights[i];
-        heights[i] = temp;
-    }
+public int[] sort(int[] heights) {
+    java.util.Arrays.sort(heights);
     return heights;
 }
-
     public int heightChecker(int[] heights) {
     Solution a=new Solution();
     int[]b=new int[heights.length];
