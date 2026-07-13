bda355e248fc54bab0981a15ff2ad2b635b80129
Ritu-Raj-Rai <rairituraj12@gmail.com>
2026-07-13 23:22:55 +0530

    Time: 8 ms (57.12%) | Memory: 46.5 MB (50.19%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+ HashMap<String,Integer>map=new HashMap<>();
+for(String num: words){
+    map.put (num,map.getOrDefault(num,0)+1);
+}
+     List<String> freqElem = new ArrayList<>();
+   List<String> keys = new ArrayList<>(map.keySet());
+  Collections.sort(keys, (a, b) -> {
+            int freqCompare = map.get(b) - map.get(a);
+            if (freqCompare == 0) {
+                return a.compareTo(b); // tie-breaker: alphabetical order
+            }
+            return freqCompare;
+        });
+    for(int i=0;i<k;i++){
+        freqElem.add(keys.get(i));
+    }
+    return freqElem;
+    }
+}
