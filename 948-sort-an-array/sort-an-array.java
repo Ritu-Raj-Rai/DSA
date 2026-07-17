a61fdd961a29afb3ae9725c06108ee994c0986dd
Ritu-Raj-Rai <rairituraj12@gmail.com>
2026-07-17 17:29:19 +0530

    Time: 28 ms (41.05%) | Memory: 75.7 MB (91.56%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        return mergeSort(nums);
+    }
+
+    private int[] mergeSort(int[] arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left, right);
+    }
+    private int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+
+        return result;
+    }
+}
