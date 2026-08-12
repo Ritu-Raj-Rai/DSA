2464a3d95a59344781255e18d859901e861c3d4c
Ritu-Raj-Rai <rairituraj12@gmail.com>
2026-08-12 14:25:32 +0530

    Time: 7 ms (99.08%) | Memory: 48.9 MB (85.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // Step 1: Sort by start time
+    
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> merged = new ArrayList<>();
@@ -10,17 +10,13 @@ class Solution {
 
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] <= current[1]) {
-                // Overlap → extend the current interval
                 current[1] = Math.max(current[1], intervals[i][1]);
             } else {
-                // No overlap → push current and reset
                 merged.add(current);
                 current = intervals[i];
             }
         }
-        // Add the last interval
         merged.add(current);
-
         return merged.toArray(new int[merged.size()][]);
     }
 }
